ad2fbb9f4eb1e511a7d091ac336330cf9949d708
MaG-NITud3 <theeparthasarathi@gmail.com>
2022-05-22 15:12:24 +0530

    holy shit something works

--- src/main/java/com/mag/Main.java
+++ src/main/java/com/mag/Main.java
@@ -1,12 +1,17 @@
 package com.mag;
 
 
-import javax.json.JsonReader;
-import java.io.FileNotFoundException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mag.Utils.ScoreMapping;
+
+import java.io.File;
+import java.io.IOException;
 
 class Main{
-    public static void main(String[] args){
-        Maker.makeList();
-        Maker.makeJson();
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        String s = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(new ScoreMapping(1));
+        System.out.println(s);
     }
 }
---
+++ src/main/java/com/mag/Utils/ScoreMapping.java
@@ -0,0 +1,15 @@
+package com.mag.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScoreMapping {
+    public String entity = "this";
+    public String condition = "minecraft:entity_scores";
+    //private int n;
+    public Map<String, Integer> scores = new HashMap<String, Integer>();
+
+    public ScoreMapping(int n) {
+        scores.put("Times", n);
+    }
+}
